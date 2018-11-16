6ba895cef091df06305de6c060c77128aa741a2a
VladislavDolin <45096621+VladislavDolin@users.noreply.github.com>
2018-11-16 15:25:08 +0300

    Add files via upload

---
+++ Test/Test/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Test
+{
+    public class Class1
+    {
+        public static double CircleArea(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static double TriangleArea(double FirstSide, double SecondSide, double ThirdSide)
+        {
+            double p = (FirstSide + FirstSide + FirstSide) / 2;
+
+            return Math.Sqrt(p * (p - FirstSide) * (p - SecondSide) * (p - ThirdSide));
+        }
+    }
+}
